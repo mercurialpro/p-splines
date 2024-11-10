be6d3862892c15d04b73f57cc8ccfe22b9acd736
ES <55274511+ESBehtev@users.noreply.github.com>
2024-11-10 03:31:58 +0300

    Переписал код интерфейса, добавил окно для лин, куб и квадр сплайнов, в целом уллучшил структуру и читаемость. В ui файле новые кнопки типов сплайнов

--- spline_types/interface.py
+++ spline_types/interface.py
@@ -1,7 +1,6 @@
 from PyQt6 import uic
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QListWidgetItem
-from spline_types.pspline import plot_spline
 from spline_types.splines import plot_p_spline
 
 class MainWindow(QMainWindow):
@@ -11,17 +10,26 @@ class MainWindow(QMainWindow):
 		# Загружаем интерфейс из файла .ui
 		uic.loadUi("spline_types/main.ui", self)
 
-		# Подключение кнопок к соответствующим методам
+		# Подключение кнопок выполнить, тест и выход к соответствующим методам
 		self.buttonExecute.clicked.connect(self.on_execute)
-		self.pushButton.clicked.connect(self.label_now)
+		self.pushButton.clicked.connect(self.handle_p_spline)
 		self.buttonExit.clicked.connect(QApplication.quit)
 
-		# Подключение выбора сплайнов к обновлению списка
-		self.buttonRSpline.toggled.connect(self.update_listWidget)
-		self.buttonZSpline.toggled.connect(self.update_listWidget)
-		self.buttonBSpline.toggled.connect(self.update_listWidget)
+		# Список радиокнопок для типов сплайнов
+		self.spline_buttons = [
+			self.p_radioButton,
+			self.linear_radioButton,
+			self.quadratic_radioButton,
+			self.cubic_radioButton,
+			self.z_radioButton,
+			self.b_radioButton
+		]
 
-		# Подключаем событие для отслеживания изменений в QListWidget
+		# Подключаем каждую радиокнопку к общему обработчику связи вида сплайна и действия
+		for button in self.spline_buttons:
+			button.toggled.connect(self.update_listWidget)
+
+		# Подключаем событие для отслеживания изменений в QListWidget с действиями
 		self.listWidget.itemChanged.connect(self.on_item_checked)
 
 		# Инициализация начальных элементов списка
@@ -34,12 +42,18 @@ class MainWindow(QMainWindow):
 
 		# Добавляем элементы в зависимости от выбранного сплайна
 		items = []
-		if self.buttonRSpline.isChecked():
-			items = ["Опция 1 для p-сплайна", "Опция 2 для p-сплайна", "Опция 3 для p-сплайна"]
-		elif self.buttonZSpline.isChecked():
-			items = ["Опция 1 для z-сплайна", "Опция 2 для z-сплайна"]
-		elif self.buttonBSpline.isChecked():
-			items = ["Опция 1 для b-сплайна", "Опция 2 для b-сплайна", "Опция 3 для b-сплайна", "Опция 4 для b-сплайна"]
+		if self.p_radioButton.isChecked():
+			items = ["Опции для p-сплайна"]#, "Опция 2 для p-сплайна", "Опция 3 для p-сплайна"]
+		elif self.linear_radioButton.isChecked():
+			items = ["Опции для линейного сплайна"]
+		elif self.quadratic_radioButton.isChecked():
+			items = ["Опции для квадратичного сплайна"]
+		elif self.cubic_radioButton.isChecked():
+			items = ["Опции для кубического сплайна"]
+		elif self.z_radioButton.isChecked():
+			items = ["Опции для z-сплайна"]#, "Опция 2 для z-сплайна"]
+		elif self.b_radioButton.isChecked():
+			items = ["Опции для b-сплайна"]#, "Опция 2 для b-сплайна", "Опция 3 для b-сплайна", "Опция 4 для b-сплайна"]
 
 		# Добавляем элементы в QListWidget с флажками
 		for item_text in items:
@@ -55,21 +69,59 @@ class MainWindow(QMainWindow):
 
 	def on_execute(self):
 		"""Проверяет, выбран ли p-сплайн, и выполняет соответствующее действие."""
-		if self.buttonRSpline.isChecked():
-			if self.buttonExample.isChecked():
-				self.label_now()
+		if self.p_radioButton.isChecked():
+			if self.ExampleButton.isChecked():
+				self.handle_p_spline()
 			elif self.VariableButton.isChecked():
 				self.run_slider_window()
 			else:
 				self.label_output.setText("Выберите пример.")
-		else:
-			self.label_output.setText("Z и B сплайны пока в разработке.")
 
-	def label_now(self):
+		elif self.linear_radioButton.isChecked():
+			self.handle_linear_spline()
+			self.development()
+		elif self.quadratic_radioButton.isChecked():
+			self.handle_quadratic_spline()
+			self.development()
+		elif self.cubic_radioButton.isChecked():
+			self.handle_cubic_spline()
+			self.development()
+
+		elif self.z_radioButton.isChecked():
+			self.handle_z_spline()
+			self.development()
+
+		elif self.b_radioButton.isChecked():
+			self.handle_b_spline()
+			self.development()
+
+		else:
+			self.label_output.setText("Выберите тип сплайна.")
+
+
+	def development(self):
+		self.label_output.setText("все, кроме p-сплайна, пока в разработке.")
+
+	def handle_p_spline(self):
 		"""Выводит сообщение и отображает график для p-сплайна."""
 		plot_p_spline()
 		self.label_output.setText("График p-сплайн был выбран и нажата кнопка Выполнить.")
 
+	def handle_linear_spline(self):
+		pass
+
+	def handle_quadratic_spline(self):
+		pass
+	def handle_cubic_spline(self):
+		pass
+
+	def handle_z_spline(self):
+		pass
+
+	def handle_b_spline(self):
+		pass
+
+	#слайдер пока реализован только для p-сплайна
 	def run_slider_window(self):
 		"""Открывает окно с ползунками для выбора значений."""
 		self.slider_window = SliderWindow()
@@ -80,7 +132,6 @@ class SliderWindow(QWidget):
 	def __init__(self):
 		super().__init__()
 		uic.loadUi('spline_types/p_variable.ui', self)
-
 		# Привязка сигналов ползунков и кнопки к методам
 		self.slider_start.valueChanged.connect(self.update_start)
 		self.slider_stop.valueChanged.connect(self.update_stop)
@@ -131,3 +182,5 @@ def start():
 	window.show()
 
 	app.exec()
+
+start()
